66eaff5442201b579607d83ac8afecbacce11de4
Saurabh <ss1998singh@gmail.com>
2021-11-27 18:19:07 +0530

    make it better

---
+++ src/main/java/com/example/demo/StudentManagementSystemApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	public void run(String... args) throws Exception {
+		
+		
+		
+		 
+		
+	}
+
+}
